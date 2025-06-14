553f4de69241505c73976d1c578eb4a6b32515bc
Yassin Hagenimana <hyassin509@gmail.com>
2025-06-14 13:05:24 +0200

    input validation and handle empty polynomial case

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -4,14 +4,13 @@
     that calculates the derivative of a polynomial
 '''
 
+
 def poly_derivative(poly):
     '''
         calculates the derivative of a polynomial
     '''
-    if not isinstance(poly, list) or not all(isinstance(x, (int, float)) for x in poly):
+    if not isinstance(poly, list) or len(poly) == 0:
         return None
-
-    if len(poly) <= 1:
+    if len(poly) == 1:
         return [0]
-
     return [poly[i] * i for i in range(1, len(poly))]
